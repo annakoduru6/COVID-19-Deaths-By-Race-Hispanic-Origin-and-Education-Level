d996b500346be5fc6ac6b157b9b9598d33d4fb03
Anna Koduru <annakoduru@gmail.com>
2023-04-29 16:38:15 -0400

    add test file

---
+++ test file/bloomdata.py
@@ -0,0 +1,19 @@
+'''This function will add one (1) to the value called.'''
+
+def increment_num(num):
+    return num + 1
+
+'''This function will multiply the number called against itself.'''
+
+def multiply_num(num):
+    return num * num
+
+'''This function will subtract one (1) to the value called.'''
+
+def subtract_num(num):
+    return num - 1
+
+'''This function will return the square root of the number called. '''
+
+def sq_rt_num(num):
+    return num ** 2
